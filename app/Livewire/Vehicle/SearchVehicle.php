6dc8b22db619cf9c3d7cfcb1e215ef7aa5c62839
Luke-H-220095075 <220095075@aston.ac.uk>
2025-05-15 15:35:38 +0100

    orders previous searches correctly

--- app/Livewire/Vehicle/SearchVehicle.php
+++ app/Livewire/Vehicle/SearchVehicle.php
@@ -13,13 +13,6 @@ class SearchVehicle extends Component
     #[Validate('required|filled', as: 'VRN')]
     public $vrn = '';
 
-    public $previousSearches = [];
-
-    public function mount(): void
-    {
-        $this->previousSearches = Auth::user()->searches()->limit(10)->get();
-    }
-
     public function search(): void
     {
         $this->validate();
@@ -28,7 +21,6 @@ class SearchVehicle extends Component
         $this->vrn = str_replace(' ', '', $this->vrn);
 
         $this->dispatch('searched', $this->vrn);
-        $this->mount();
     }
 
     #[On('valid')]
