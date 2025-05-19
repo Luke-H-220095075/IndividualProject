0a0f31147b4af254609a3325bab7ffffb3651481
Luke-H-220095075 <220095075@aston.ac.uk>
2025-05-19 17:13:10 +0100

    adds relationship between customer and vehicles

--- resources/views/livewire/customer/customer-vehicles.blade.php
+++ resources/views/livewire/customer/customer-vehicles.blade.php
@@ -10,7 +10,7 @@
 
     <flux:separator class="my-2"></flux:separator>
 
-    <flux:heading class="text-center">Customer Vehicle(s)</flux:heading>
+    <flux:heading class="text-center">All Vehicles</flux:heading>
     <div class="text-center w-full h-44 grid grid-cols-2 gap-y-3 overflow-scroll py-2">
         @foreach($vehicles as $vehicle)
             <flux:badge variant="solid" color="yellow" class="justify-self-center self-center w-fit h-fit cursor-pointer">
