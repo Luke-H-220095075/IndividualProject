7d59a86482b1a283681bec11d6a4ce473a6ee033
Luke-H-220095075 <220095075@aston.ac.uk>
2025-05-28 17:15:37 +0100

    initial bookings display

--- app/Livewire/Booking/BookingTable.php
+++ app/Livewire/Booking/BookingTable.php
@@ -2,10 +2,18 @@
 
 namespace App\Livewire\Booking;
 
+use App\Models\Booking;
 use Livewire\Component;
 
 class BookingTable extends Component
 {
+    public $bookings = [];
+
+    public function mount(): void
+    {
+        $this->bookings = Booking::all();
+    }
+
     public function render()
     {
         return view('livewire.booking.booking-table');
--- resources/views/livewire/booking/booking-table.blade.php
+++ resources/views/livewire/booking/booking-table.blade.php
@@ -1,3 +1,32 @@
-<div>
-    {{-- In work, do what you enjoy. --}}
+@use(Carbon\Carbon)
+<div class="text-xs text-center p-2 overflow-auto h-full">
+    <style>
+        label {
+            font-weight: bold;
+        }
+    </style>
+
+    <div class="grid grid-cols-4 gap-y-2 my-2 items-center">
+        <label for="type">Type</label>
+        <label for="description">Description</label>
+        <label for="date">Date</label>
+        <label for="time">Time</label>
+
+        <hr class="col-span-full mx-8">
+
+        @if($bookings)
+            @foreach($bookings as $booking)
+                <p id="type">{{ $booking->type }}</p>
+                <p id="description">{{ $booking->description }}</p>
+                <p id="date">{{ $booking->date }}</p>
+                <p id="time">{{ Carbon::parse($booking->time)->format('H:i') }}</p>
+
+                @if(!$loop->last)
+                    <hr class="col-span-full mx-8 border-gray-500">
+                @endif
+            @endforeach
+        @else
+            <p class="col-span-full">No bookings found</p>
+        @endif
+    </div>
 </div>
