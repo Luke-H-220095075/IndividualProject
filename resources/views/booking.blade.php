075ab569386530e073fa69c93a4d9c718c4163e7
Luke-H-220095075 <220095075@aston.ac.uk>
2025-05-28 17:00:32 +0100

    adds booking table

--- resources/views/booking.blade.php
+++ resources/views/booking.blade.php
@@ -2,11 +2,11 @@
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         <div class="grid auto-rows-min gap-4">
             <div class="relative overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <livewire:invoice.create-invoice></livewire:invoice.create-invoice>
+                <livewire:booking.create-booking></livewire:booking.create-booking>
             </div>
         </div>
         <div class="relative h-full flex-1 rounded-xl border border-neutral-200 dark:border-neutral-700">
-            <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+            <livewire:booking.booking-table></livewire:booking.booking-table>
         </div>
     </div>
 </x-layouts.app>
