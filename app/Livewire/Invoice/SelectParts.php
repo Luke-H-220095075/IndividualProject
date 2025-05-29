5e3ec5bcaf8760f8a38cee0af2c5e240c57eb409
Luke-H-220095075 <220095075@aston.ac.uk>
2025-05-29 16:01:20 +0100

    can add parts to invoice

--- app/Livewire/Invoice/SelectParts.php
+++ app/Livewire/Invoice/SelectParts.php
@@ -2,10 +2,27 @@
 
 namespace App\Livewire\Invoice;
 
+use App\Models\Part;
 use Livewire\Component;
 
 class SelectParts extends Component
 {
+    public $parts = [];
+    public $partId = '';
+    public $selectedParts = [];
+
+    public function mount(): void
+    {
+        $this->parts = Part::all();
+    }
+
+    public function addPart(): void
+    {
+        $part = Part::query()->find($this->partId);
+
+        $this->selectedParts[] = $part;
+    }
+
     public function render()
     {
         return view('livewire.invoice.select-parts');
--- resources/views/livewire/invoice/select-parts.blade.php
+++ resources/views/livewire/invoice/select-parts.blade.php
@@ -1,3 +1,31 @@
-<div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+<div class="p-2">
+    <style>
+        label {
+            font-weight: bold;
+        }
+    </style>
+
+    <flux:select wire:model="partId" wire:change="addPart" label="Part">
+        <flux:select.option value="{{ null }}"></flux:select.option>
+        @foreach($parts as $part)
+            <flux:select.option value="{{ $part->id }}">{{ $part->type }}</flux:select.option>
+        @endforeach
+    </flux:select>
+
+    <flux:separator class="my-2"></flux:separator>
+
+    <div class="grid grid-cols-2 text-sm text-center">
+        <label for="type">Type</label>
+        <label for="price">Price</label>
+
+        @if($selectedParts)
+            @foreach($selectedParts as $part)
+                <p id="type">{{ $part->type }}</p>
+                <p id="price">{{ $part->price }}</p>
+            @endforeach
+        @else
+            <p class="col-span-2">No parts selected</p>
+        @endif
+
+    </div>
 </div>
